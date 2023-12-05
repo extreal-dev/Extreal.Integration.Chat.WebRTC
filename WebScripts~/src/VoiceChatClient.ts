41c294878fa06e97c09f6d6d79ec03860cd57c6f
sus-yoshikane-t <yoshikane-t@m.sus-g.co.jp>
2023-12-05 15:31:56 +0900

    Add mic volume set methods for WebGL

--- WebScripts~/src/VoiceChatClient.ts
+++ WebScripts~/src/VoiceChatClient.ts
@@ -11,16 +11,18 @@ class VoiceChatClient {
     private readonly getPeerClient: PeerClientProvider;
     private readonly hasMicrophone: boolean;
 
+    private peerConnectionIds: Set<string>;
     private inStreams: Map<string, MediaStream>;
     private inTracks: Map<string, MediaStreamTrack>;
     private outAudios: Map<string, HTMLAudioElement>;
     private outStreams: Map<string, MediaStream>;
 
+    private inGainNodes: Map<string, GainNode>;
+
     private mute: boolean;
     private micVolume: number;
 
     private audioContext: AudioContext;
-    private micGainNode: GainNode;
 
     constructor(voiceChatConfig: VoiceChatConfig, getPeerClient: PeerClientProvider, hasMicrophone: boolean) {
         this.isDebug = voiceChatConfig.isDebug;
@@ -29,6 +31,7 @@ class VoiceChatClient {
         this.micVolume = 1.0;
         this.getPeerClient = getPeerClient;
         this.hasMicrophone = hasMicrophone;
+        this.peerConnectionIds = new Set();
         this.inStreams = new Map();
         this.inTracks = new Map();
         this.outAudios = new Map();
@@ -39,11 +42,11 @@ class VoiceChatClient {
             console.log(hasMicrophone ? "Microphone found" : "Microphone not found");
         }
         this.audioContext = new AudioContext();
-        this.micGainNode = this.audioContext.createGain();
+        this.inGainNodes = new Map<string, GainNode>();
     }
 
     private createPc = async (id: string, isOffer: boolean, pc: RTCPeerConnection) => {
-        if (this.outAudios.has(id)) {
+        if (this.peerConnectionIds.has(id)) {
             return;
         }
 
@@ -51,14 +54,19 @@ class VoiceChatClient {
             console.log(`New MediaStream: id=${id}`);
         }
 
+        this.peerConnectionIds.add(id);
+
         const client = this;
 
         if (this.hasMicrophone) {
-            const inStream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            const micStream = await navigator.mediaDevices.getUserMedia({ audio: true });
 
-            var source = client.audioContext.createMediaStreamSource(inStream);
-            source.connect(client.micGainNode);
-            client.micGainNode.connect(client.audioContext.destination);
+            const source = client.audioContext.createMediaStreamSource(micStream);
+            const gainNode = client.audioContext.createGain();
+            const destinationNode = client.audioContext.createMediaStreamDestination();
+            const inStream = destinationNode.stream;
+            source.connect(gainNode);
+            gainNode.connect(destinationNode);
 
             client.inStreams.set(id, inStream);
             const inTrack = inStream.getAudioTracks()[0];
@@ -66,7 +74,8 @@ class VoiceChatClient {
             pc.addTrack(inTrack, inStream);
             inTrack.enabled = !this.mute;
 
-            client.micGainNode.gain.value = client.micVolume;
+            gainNode.gain.value = client.micVolume;
+            client.inGainNodes.set(id, gainNode);
         }
 
         const outAudio = new Audio();
@@ -98,6 +107,12 @@ class VoiceChatClient {
             outStream.getTracks().forEach((track) => track.stop());
             this.outStreams.delete(id);
         }
+
+        const gainNode = this.inGainNodes.get(id);
+        if (gainNode)
+        {
+            this.inGainNodes.delete(id);
+        }
     };
 
     public clear = () => {
@@ -119,7 +134,9 @@ class VoiceChatClient {
 
     public setMicVolume = (volume: number) => {
         this.micVolume = volume;
-        this.micGainNode.gain.setValueAtTime(this.micVolume, this.audioContext.currentTime);
+        this.inGainNodes.forEach(gainNode => {
+            gainNode.gain.setValueAtTime(this.micVolume, this.audioContext.currentTime);
+        })
     }
 }
 
